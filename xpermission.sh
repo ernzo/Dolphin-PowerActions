54a6893c17b9b08f7874a848671c4dfe618b8396
ernzo <ernzzzz@gmail.com>
2024-08-30 01:34:10 +0200

    Update xpermission.sh
    Added check to retry action with sudo if adding permission fails.

--- xpermission.sh
+++ xpermission.sh
@@ -2,5 +2,17 @@
 # Add execute permission to file/s
 for file in "$@"; do
     chmod +x "$file"
-    echo "Updated Execute Permission for $file"
+
+    if [ $? -ne 0 ]; then
+        echo "Failed updating permission for $file, retry with sudo..."
+        sudo chmod +x "$file"
+
+        if [ $? -eq 0 ]; then
+            echo "Updated Execute Permission for $file"
+        else
+            echo "Failed updating permission for $file with sudo"
+        fi
+    else
+        echo "Updated Execute Permission for $file"
+    fi
 done
