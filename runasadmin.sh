c1000075b8d6bf8864502385987bec6031d2aa03
ernzo <ernzzzz@gmail.com>
2024-09-05 19:46:37 +0200

    Update runasadmin.sh

--- runasadmin.sh
+++ runasadmin.sh
@@ -2,17 +2,17 @@
 
 # Check file
 if [ -z "$1" ]; then
-  echo "No file provided. Usage: runasadmin.sh <file>"
-  exit 1
+    echo "No file provided. Usage: runasadmin.sh <file>"
+    exit 1
 fi
 
 FILE="$1"
 
 # File exists?
 if [ ! -f "$FILE" ]; then
-  echo "File does not exist: $FILE"
-  exit 1
+    echo "File does not exist: $FILE"
+    exit 1
 fi
 
 # Run file as admin, preserve environment, keep the terminal open
-x-terminal-emulator -e "sudo -E bash \"$FILE\"; exec bash"
+sudo -E bash "$FILE"; exec bash
